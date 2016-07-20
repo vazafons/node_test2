7062373823b89e1767e29cf2fcb8ff2bafffe048
Vitor Vaz Afonso <vitor.vazafonso@epfl.ch>
2016-07-20 17:20:43 +0200

    Afficher la date

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 'use strict'
 var express = require('express');
 var app = express();
+var http = require('http');
+var moment = require('moment');
+var hour = moment().format('LLLL');
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
-app.listen(8000);
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end(hour);
+});
+
+server.listen(8000); 
