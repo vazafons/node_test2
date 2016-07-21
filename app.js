00d42756f2dc49e3e715c10499875844950d4add
Vitor Vaz Afonso <vitor.vazafonso@epfl.ch>
2016-07-21 11:25:06 +0200

    afficher la date avec une fonction

--- app.js
+++ app.js
@@ -7,9 +7,34 @@ var hour = moment().format('LLLL');
 
 //app.use(express.static('public'));
 
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    
+
+    var min  = date.getMinutes();
+    
+
+    var sec  = date.getSeconds();
+    
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+   
+
+    var day  = date.getDate();
+    
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
+
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end(hour);
+  res.end(getDateTime());
 });
 
 server.listen(8000); 
