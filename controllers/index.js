e1a5b13547d93d42f05312ad6eda3391ac0b040f
vazafons <vitor.vazafonso@hotmail.ch>
2016-08-11 13:11:44 +0200

    Afficher des données depuis un fichier Json

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var dateDujour = require('../models/dateDujour');
 
+var fs = require('fs');
+var obj;
+fs.readFile('fruits.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data);
+  kiwis = obj.fruits[0].kiwis;
+  //console.log(kiwis);
+});
+
 /* GET home page.*/
 router.get('/', function (req, res, next) {
   res.render('index', dateDujour());
@@ -9,6 +18,11 @@ router.get('/', function (req, res, next) {
 
 router.get('/test', function (req, res, next) {
   res.render('test',{title: 'youpidouo'});
+});
+
+
+router.get('/fruits', function (req, res, next) {
+  res.render('fruits',{title: kiwis});
 });
 
 module.exports = router; 
