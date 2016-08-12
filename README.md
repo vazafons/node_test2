46c85e92943f098ec5299d5afc55abafada20eec
vazafons <vitor.vazafonso@hotmail.ch>
2016-08-12 14:59:51 +0200

    ajout sequelize + tests avec data  d'une DB

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 - [x] Rendre un fichier html au client (static, express)
 - [x] Rendre une page web (dynamique) ex : heure (Express,pug)
 - [x] Créer un template pour centraliser le format de rendu (Pages multiples avec même header...)
-- [ ] Afficher des données static (En dur -> puis fichier Json) formaté html (table d'une liste de valeur)
+- [x] Afficher des données static (En dur -> puis fichier Json) formaté html (table d'une liste de valeur)
 - [ ] Afficher des données depuis une BDD (sequelize, mysql)
 - [ ] Structurer le code de manière modulaire pour séparer les responsabilités de chaque modules
--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var dateDujour = require('../models/dateDujour');
 var NbDeFruits = require('../models/NbDeFruits');
 
+var Sequelize = require('sequelize');
+
+var connection = new Sequelize('testsequelize', 'root', 'password');
+
 /*
 var fs = require('fs');
 var obj;
@@ -25,7 +29,15 @@ router.get('/test', function (req, res, next) {
 
 
 router.get('/fruits', function (req, res, next) {
-  res.render('fruits', NbDeFruits());
+  //res.render('fruits', NbDeFruits());
+  res.render('fruits', {title: connection.sync().then(function () {
+
+connection.query("SELECT * FROM testsequelize.sensorvalues LIMIT 2;").then(function(projects) {
+ return (console.log(projects));
+})
+
+})
+ });
 });
 
 module.exports = router; 
