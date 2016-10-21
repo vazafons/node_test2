097fdbe80a196bf7570b7bb20f41578626761a5d
vazafons <vitor.vazafonso@hotmail.ch>
2016-10-21 10:51:45 +0200

    Installation de Bootstrap 3 Datepicker v4

--- controllers/index.js
+++ controllers/index.js
@@ -1,33 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var dateDujour = require('../models/dateDujour');
-var NbDeFruits = require('../models/NbDeFruits');
+
 var Sequelize = require('sequelize');
 
 var connection = new Sequelize('testsequelize', 'root', 'password');
 
 var Post = connection.define('sensorvalues', {});
-//var connection = new Sequelize('testsequelize', 'insertValues', 'pass');
-
-/*
-var fs = require('fs');
-var obj;
-fs.readFile('fruits.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  obj = JSON.parse(data);
-  kiwis = obj.fruits[0].kiwis;
-  //console.log(kiwis);
-});
-*/
 
 /* GET home page.*/
 router.get('/', function (req, res, next) {
-  res.render('index', dateDujour());
+  res.render('index', {
+    title : "test"
+  });
 });
 
-router.get('/graph', function (req, res, next) {
-  res.render('graph');
-});
 
 router.get('/sensor', function (req, res, next) {
   var sensId = req.query.selectSensor;
@@ -73,43 +59,6 @@ router.get('/sensors', function (req, res, next) {
       res.render('sensors', {
         title: projects[0]
       });
-/*
-      
-      var structuredValues = [];
-
-      projects[0].map(function (currentSensorItem) {
-        var existingSensorObjs = structuredValues.filter(function (sensor) { return sensor.title === currentSensorItem.Sensors_SID });
-        var existingSensor;
-
-        if (existingSensorObjs.length > 0) {
-          existingSensor = existingSensorObjs[0];
-        } else {
-          existingSensor = {
-            title: currentSensorItem.Sensors_SID,
-            values: []
-          };
-
-          //existingSensor[currentSensorItem.Sensors_SID] = {};
-        }
-
-        existingSensor.values.push({
-          sensVal: currentSensorItem.Value,
-          date: currentSensorItem.CreatedAt
-        });
-
-        if (existingSensorObjs.length <= 0) {
-          structuredValues.push(existingSensor);
-        }
-      });
-
-      res.render('sensors', {
-        title: projects[0][0].Sensors_SID,
-        sensId: projects[0][0].Sensors_SID,
-        sensVal: projects[0][0].Value,
-        sensors: projects[0],
-        values: structuredValues
-      });
-      */
     });
 });
 
